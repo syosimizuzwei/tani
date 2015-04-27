1160ed6715413447d15b58c62db8bcb5d26922fa
Shotaro <futatumeno@gmail.com>
2015-04-27 22:13:10 +0900

    ああ

---
+++ tani/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  tani
+//
+//  Created by Syotaro Shimizu on 2015/04/27.
+//  Copyright (c) 2015年 Syotaro Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
